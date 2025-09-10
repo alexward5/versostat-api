c5b6b98c4aeb51ecd84c8f6cebedcce69687aa8b
Alex Ward <alexanderward5@gmail.com>
2025-09-10 15:07:11 -0600

    conditionally use ssl cert in prod

--- src/pg.ts
+++ src/pg.ts
@@ -1,4 +1,19 @@
+import fs from "fs";
 import { Pool } from "pg";
+import { ConnectionOptions } from "tls";
+
+let ssl: boolean | ConnectionOptions = false;
+if (process.env.NODE_ENV === "production") {
+    ssl = {
+        rejectUnauthorized: true,
+        ca: fs
+            .readFileSync(
+                process.env.PGSSLROOTCERT ||
+                    "/etc/ssl/certs/rds-global-bundle.pem"
+            )
+            .toString(),
+    };
+}
 
 const pool = new Pool({
     host: process.env.dbhost,
@@ -6,6 +21,7 @@ const pool = new Pool({
     database: process.env.database,
     user: process.env.dbuser,
     password: process.env.dbpassword,
+    ssl: ssl,
     max: 20,
     idleTimeoutMillis: 15000,
     connectionTimeoutMillis: 10000,
